228bb9b8511dab4082a501bd42e0755f2d960da2
Sarah <sarahjeannesexton@live.com>
2024-10-16 16:11:41 -0700

    Ask for user to input either an ip address or a hostname of their choosing

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,43 @@
 mod dns_lookup;
 pub use crate::dns_lookup::*;
 use std::net::IpAddr;
+use std::io::{self, Write};
 
 fn main() {
-    // Example IP address
-    let ip_address: IpAddr = "8.8.8.8".parse().unwrap();
-    match ip_to_hostname(ip_address) {
-        Ok(hostname) => {
-            println!("let ip_address: IpAddr = {:?}", ip_address);
-            println!("Hostname: {}", hostname);
-        }
-        Err(e) => {
-            eprintln!("{}", e);
-        }
-    }
+    let mut input = String::new();
 
-    // Example hostname
-    let hostname = "example.com";
-    match hostname_to_ip(hostname) {
-        Ok(ips) => {
-            for ip_address in ips {
-                println!("let hostname = {}", hostname);
-                println!("IP address: {}", ip_address);
+    // Ask the user for an IP address or hostname
+    println!("Enter an IP address or a hostname:");
+
+    // Read the user input
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut input).unwrap();
+    let input = input.trim();
+
+    // Try to parse the input as an IP address
+    if let Ok(ip_address) = input.parse::<IpAddr>() {
+        match ip_to_hostname(ip_address) {
+            Ok(hostname) => {
+                println!("let ip_address: IpAddr = {:?}", ip_address);
+                println!("Hostname: {}", hostname);
+            }
+            Err(e) => {
+                eprintln!("{}", e);
             }
         }
-        Err(e) => {
-            eprintln!("{}", e);
+    } else {
+        // Treat the input as a hostname
+        let hostname = input;
+        match hostname_to_ip(hostname) {
+            Ok(ips) => {
+                for ip_address in ips {
+                    println!("let hostname = {}", hostname);
+                    println!("IP address: {}", ip_address);
+                }
+            }
+            Err(e) => {
+                eprintln!("{}", e);
+            }
         }
     }
 }
